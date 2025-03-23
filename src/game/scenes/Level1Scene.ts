fe389a6e4754a4b6bf4946ecbb23c26726350455
Paul Goldston <paulygoldston@gmail.com>
2025-03-23 16:04:26 +1000

    Fix background image reference: use Background2.jpg instead of Background.png

--- src/game/scenes/Level1Scene.ts
+++ src/game/scenes/Level1Scene.ts
@@ -230,7 +230,7 @@ export class Level1Scene extends Phaser.Scene {
       });
 
       // Load background
-      this.load.image('background', 'game/Background/Background.png')
+      this.load.image('background', 'game/Background/Background2.jpg')
         .on('filecomplete', () => {
           gameDebugger.info('Background loaded');
         })
